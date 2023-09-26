f1dc479da106889bfd4a29b349ff3adaeaf6f0f0
Joe Tavinor <joe.tavinor@appscore.net>
2023-09-26 20:48:46 +0100

    set up routing

---
+++ src/AddCost.js
@@ -0,0 +1,20 @@
+import { Box } from "@mui/material";
+
+function AddCost() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Box width={300} height={200} border={1} p={2}>
+        <h1>Hello</h1>
+      </Box>
+    </div>
+  );
+}
+
+export default AddCost;
